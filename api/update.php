87385f5beea6786f341958c33d001b8dc62950e8
raninovializa <57649467+raninovializa@users.noreply.github.com>
2021-05-20 21:31:53 +0700

    Update update.php

--- api/update.php
+++ api/update.php
@@ -8,9 +8,9 @@ $Jumlah_Barang = @$_POST['Jumlah_Barang'];
 
 $data = [];
 
-$query = mysqli_query($kon, "UPDATE 'barang' SET 
-'Nama_Barang'='". $Nama_Barang ."', 'Jumlah_Barang'='". $Jumlah_Barang ."' 
-WHERE 'ID'='". $ID ."'");
+$query = mysqli_query($kon, "UPDATE barang SET 
+Nama_Barang='$Nama_Barang', Jumlah_Barang='$Jumlah_Barang' 
+WHERE ID='$ID'");
 
 if($query){
 	$status = true;
