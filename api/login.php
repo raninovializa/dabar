7acd70d7be8e9baf963e7a0c02b3decd1daa957c
raninovializa <57649467+raninovializa@users.noreply.github.com>
2021-05-20 21:29:46 +0700

    Update login.php

--- api/login.php
+++ api/login.php
@@ -7,10 +7,9 @@ $Password = md5(@$_POST['Password']);
 
 $data = [];
 
-$query = mysqli_query($kon, "SELECT * FROM 'admin' WHERE 'Username' = '".
-$Username ."'
+$query = mysqli_query($kon, "SELECT * FROM admin WHERE Username = '$Username'
 &&
-'Password' = '". $Password ."'");
+Password = '$Password'");
 
 if($query){
 	$status = true;
